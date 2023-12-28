7ad07b0c56ade18e81f51d320260c1a9538ceca6
Nick <noliveira95@outlook.com>
2023-12-28 12:51:37 -0800

    refactor: set up ListItem component, refactor styles

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -4,11 +4,11 @@ import {
   StyleSheet,
   Text,
   FlatList,
-  View,
   StatusBar,
   ImageBackground,
 } from "react-native";
-import { Feather } from "@expo/vector-icons";
+
+import ListItem from "../components/ListItem";
 
 const DATA = [
   {
@@ -49,22 +49,10 @@ const DATA = [
   },
 ];
 
-const Item = (props) => {
-  const { dt_txt, min, max, condition } = props;
-  return (
-    <View style={styles.item}>
-      <Feather name="sun" size={50} color="white" />
-      <Text style={styles.date}>{dt_txt}</Text>
-      <Text style={styles.temp}>{min}</Text>
-      <Text style={styles.temp}>{max}</Text>
-    </View>
-  );
-};
-
 const UpcomingWeather = () => {
   const renderItem = ({ item }) => {
     return (
-      <Item
+      <ListItem
         dt_txt={item.dt_txt}
         min={item.main.temp_min}
         max={item.main.temp_max}
@@ -72,10 +60,11 @@ const UpcomingWeather = () => {
       />
     );
   };
+  const { container, image } = styles;
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={container}>
       <ImageBackground
-        style={styles.image}
+        style={image}
         source={require("../../assets/upcoming-bg.jpg")}
       >
         <Text>Upcoming Weather</Text>
@@ -94,24 +83,6 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight || 0, // This is to get the height of the status bar
     backgroundColor: "royalblue",
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-    borderWidth: 5,
-    backgroundColor: "pink",
-  },
-  date: {
-    color: "white",
-    fontSize: 15,
-  },
-  temp: {
-    color: "white",
-    fontSize: 20,
   },
   image: {
     flex: 1,
