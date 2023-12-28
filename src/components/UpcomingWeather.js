a2597f995fcac82122a53c8d616ae42ecf0ff8bd
Nick <noliveira95@outlook.com>
2023-12-27 16:35:05 -0800

    feat: set up FlatList with placeholder data

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,7 +1,58 @@
 import React from "react";
+import { SafeAreaView, StyleSheet, Text, FlatList } from "react-native";
+
+const DATA = [
+  {
+    dt_txt: "2021-06-24 12:00:00",
+    main: {
+      temp_min: 30,
+      temp_max: 55,
+    },
+    weather: [
+      {
+        main: "Clear",
+      },
+    ],
+  },
+  {
+    dt_txt: "2021-06-25 12:00:00",
+    main: {
+      temp_min: 32,
+      temp_max: 58,
+    },
+    weather: [
+      {
+        main: "Clear",
+      },
+    ],
+  },
+  {
+    dt_txt: "2021-06-26 12:00:00",
+    main: {
+      temp_min: 28,
+      temp_max: 50,
+    },
+    weather: [
+      {
+        main: "Clouds",
+      },
+    ],
+  },
+];
 
 const UpcomingWeather = () => {
-  return <div>UpcomingWeather</div>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Upcoming Weather</Text>
+      <FlatList data={DATA} renderItem={() => {}}></FlatList>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default UpcomingWeather;
