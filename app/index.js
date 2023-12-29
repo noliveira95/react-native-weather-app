8a9f0e1dfec47101451e51fa00783a2f414bdd17
Nick <noliveira95@outlook.com>
2023-12-29 13:49:55 -0800

    build: migrate to Expo Router

--- app/index.js
+++ app/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import CurrentWeather from "./src/screens/CurrentWeather";
-import UpcomingWeather from "./src/screens/UpcomingWeather";
-import City from "./src/screens/City";
+import CurrentWeather from "../src/screens/CurrentWeather";
 
 const App = () => {
   return (
