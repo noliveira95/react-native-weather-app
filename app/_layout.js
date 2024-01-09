f35e1c434d202725d7f2ca8d3553d53f07c570da
Nick <noliveira95@outlook.com>
2024-01-09 14:37:19 -0800

    feat: set up location permission request

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Tabs } from "expo-router";
 import { Feather } from "@expo/vector-icons";
 import { ActivityIndicator, View, StyleSheet } from "react-native";
 
+import * as Location from "expo-location";
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
 const RootLayout = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setError("Permission to access location was denied");
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  if (location) {
+    console.log(location);
+  }
+
+  // let text = "Waiting..";
+  // if (error) {
+  //   text = error;
+  // } else if (location) {
+  //   text = JSON.stringify(location);
+  // }
+
   if (isLoading) {
     return (
       <View style={styles.container}>
