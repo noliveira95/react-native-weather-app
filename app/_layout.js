efd1ac0783ed3b55fd22fb40a2dd3b7c5ac3795a
Nick <noliveira95@outlook.com>
2024-01-15 10:32:01 -0800

    refactor: remove hook code

--- app/_layout.js
+++ app/_layout.js
@@ -2,47 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Tabs } from "expo-router";
 import { Feather } from "@expo/vector-icons";
 import { ActivityIndicator, View, StyleSheet } from "react-native";
-import * as Location from "expo-location";
-import { WEATHER_API_KEY } from "@env";
+import { useGetWeather } from "../src/hooks/useGetWeather";
 
 const RootLayout = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
-  const [error, setError] = useState(null);
-  const [weatherData, setWeatherData] = useState([]);
+  let [isLoading, error, weatherData] = useGetWeather();
 
-  const fetchWeatherData = async () => {
-    try {
-      const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${WEATHER_API_KEY}`
-      );
-      const data = await res.json();
-      setWeatherData(data);
-    } catch (err) {
-      setError("There was a problem retrieving the weather data");
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setError("Permission to access location was denied");
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLat(`${location.coords.latitude}`);
-      setLong(`${location.coords.longitude}`);
-      await fetchWeatherData();
-    })();
-  }, [lat, long]);
-
-  if (weatherData) {
-    console.log(weatherData);
-  }
+  console.log(isLoading, error, weatherData);
 
   if (isLoading) {
     return (
